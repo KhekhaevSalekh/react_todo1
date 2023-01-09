eb7b817a0b7fb2d7c1cc028227101e1228a2c9d2
KhekhaevSalekh <saleh_1991@mail.ru>
2023-01-09 15:55:29 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import { v4 } from 'uuid'
+import { randomColor } from 'randomcolor'
+import Dragable from 'react-draggable'
 
 function App() {
+
+  let [note, noteSet] = useState('')
+  let [notes, notesSet] = useState(
+    JSON.parse(localStorage.getItem('notes')) || []
+  )
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes))
+  }, [notes])
+  const enterButton = () => {
+    console.log(note)
+    console.log(notes)
+    notesSet([...notes, note])
+    noteSet('')
+    console.log(note)
+    console.log(notes)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='wrapper'>
+      <input
+        placeholder='Enter something ...'
+        value={note}
+        onChange={(e) => noteSet(e.target.value)}
+        type='text' />
+      <button
+        onClick={enterButton}
+      >ENTER</button>
+
     </div>
-  );
+    <div className='todo_item'>
+    {notes.map((note, index) => {
+        return ( 
+          <Dragable><div>{note}</div></Dragable>
+          
+)
+      })}
+      </div>
+    </>
+  )
 }
 
 export default App;
